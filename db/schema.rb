db812b0f0b6d34aad204d35599bfffd199ca7c1c
Tim Cheah <tim.cheah.89@gmail.com>
2018-05-23 12:35:13 +0900

    added category to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_032914) do
+ActiveRecord::Schema.define(version: 2018_05_23_033324) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_05_23_032914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone_number"
+    t.string "category"
   end
 
   create_table "reviews", force: :cascade do |t|
